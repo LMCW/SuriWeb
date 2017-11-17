67d112d83bada83f0e32f2757f92315abb5c4bbd
陈志鹏 <chenzhipeng@chenzhipengdeMacBook-Pro.local>
2017-11-17 15:10:52 +0800

    app.py create

---
+++ app/app.py
@@ -0,0 +1,24 @@
+import os
+
+from flask import Flask
+from flask import request
+from flask import redirect
+from flask import render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+	return redirect('/login')
+
+@app.route('/login', methods=['POST','GET'])
+def login():
+	error = None
+	return render_template('login.html')
+
+@app.route('/index')
+def index():
+	return 'index'
+
+if __name__ == '__main__':
+	app.run(debug=True)
