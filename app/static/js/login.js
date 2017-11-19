c22d8463dc84830a2b5813418a572b0e00445a36
winsena <nanhaiwinsen@163.com>
2017-11-19 10:21:29 +0800

    前端的一些界面

--- app/static/js/login.js
+++ app/static/js/login.js
@@ -1,5 +1,7 @@
 function validate(){
+	alert("施工中");
 	// alert(document.getElementById("userName").value) 用这种方式来根据id获取值，注意最后的.value
+	
 	if (document.getElementById("userName").value == ""){
 		alert("You must enter your userName.");
 		return false;
