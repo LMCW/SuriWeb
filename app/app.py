855a94a43285a5f2a0dd59e8c0809c5f1cbd6a10
liuzf13 <liuzf13@163.com>
2017-11-19 13:46:10 +0800

    登录的前后端交互以及数据库查找

--- app/app.py
+++ app/app.py
@@ -5,17 +5,43 @@ from flask import request
 from flask import redirect
 from flask import render_template
 
-from database.database.py import database
+from database.database import database
 
 app = Flask(__name__)
+
+def request_to_dict(http_request):
+    data_dict = dict(http_request.form)
+    print data_dict
+    for key in data_dict.keys():
+        data_dict[key] = data_dict[key][0]
+    return data_dict
+
 
 @app.route('/')
 def home():
 	return redirect('/index')
 
+
 @app.route('/login', methods=['POST','GET'])
 def login():
 	error = None
+	#print request.form
+	#print request.method
+	if request.method == 'POST':
+		data=request_to_dict(request)
+		username = data['username']
+		password = data['password']
+
+		# check if these infomation are correct in the database
+		result=database.select_by_id("select * from user where username='%s' and password='%s'"%(username,password))
+		#result=database.select_all('user')
+		if not result:
+			# wrong username or password
+			print 'Fails login'
+		else:
+			print "Login Successful"
+			return redirect('/index')
+
 	return render_template('login.html')
 
 @app.route('/index')
--- app/static/js/login.js
+++ app/static/js/login.js
@@ -1,16 +1,15 @@
 function validate(){
-	alert("施工中");
 	// alert(document.getElementById("userName").value) 用这种方式来根据id获取值，注意最后的.value
 	
 	if (document.getElementById("userName").value == ""){
 		alert("You must enter your userName.");
 		return false;
-	} else if (document.getElementById("password").value == "") {
+	} else if (document.getElementById("passWord").value == "") {
 		alert("You must enter your password.");
 		return false;
 	}
 	else{
-		// 传递给后端
+		return true;
 		
 	}
 }
