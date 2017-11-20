0df341fc6b917ed273896fd7c17f99557b81a3a9
wu-yy <wuyy17@mails.tsinghua.edu.cn>
2017-11-20 11:51:07 +0800

    数据库文件sql 更新

--- suriweb.sql
+++ suriweb.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: 2017-11-19 05:30:25
+-- Generation Time: 2017-11-20 04:50:37
 -- 服务器版本： 5.6.17
 -- PHP Version: 5.5.12
 
@@ -30,15 +30,27 @@ CREATE TABLE IF NOT EXISTS `user` (
   `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '用户的ID',
   `username` varchar(50) CHARACTER SET utf8 DEFAULT NULL COMMENT '用户名',
   `password` varchar(50) CHARACTER SET utf8 DEFAULT NULL COMMENT '密码',
+  `mail` varchar(30) DEFAULT NULL COMMENT '邮箱',
+  `info` text COMMENT '个人简介',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8mb4 COMMENT='用户名登录和注册' AUTO_INCREMENT=2 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8mb4 COMMENT='用户名登录和注册' AUTO_INCREMENT=12 ;
 
 --
 -- 转存表中的数据 `user`
 --
 
-INSERT INTO `user` (`id`, `username`, `password`) VALUES
-(1, 'root', 'root');
+INSERT INTO `user` (`id`, `username`, `password`, `mail`, `info`) VALUES
+(1, 'root', 'root', NULL, NULL),
+(2, 'wuyy3', 'wuyy', NULL, NULL),
+(3, 'wuyy3', 'wuyy', NULL, NULL),
+(4, 'wuyy78', 'wuyy78', NULL, NULL),
+(5, 'wuyy78', 'wuyy78', NULL, NULL),
+(6, 'wuyy78', 'wuyy78', 'wuyy@126.com', '清华大学'),
+(7, 'hello', 'de', 'adaas', 'ed'),
+(8, 'gr', 'gr', 'sgs', 'csd'),
+(9, 'wuyy758', 'wuyy758', 'wuyy@126.com', '清华大学'),
+(10, 'das', 'ds', 'ds', 'ds'),
+(11, 'ds', 'ds', 'ds', '哈哈');
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
